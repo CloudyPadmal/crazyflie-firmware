69a79f178cfd8275f2ff82b4bbdec231c3e8c6ba
CloudyPadmal <blog.padmal@gmail.com>
2018-09-25 17:01:30 +0530

    tested all

--- src/deck/drivers/src/oa.c
+++ src/deck/drivers/src/oa.c
@@ -20,31 +20,31 @@
 static bool isInit = false;
 static bool isTested = false;
 
-#define OA_PIN_LIGHT  PCA9555_P00 // OK
-#define OA_PIN_NORTH  PCA9555_P02 // OK
-#define OA_PIN_NEAST  PCA9555_P03 // OK
-#define OA_PIN_EFLAT  PCA9555_P04 // OK
-#define OA_PIN_EDOWN  PCA9555_P05 // NOT CONNECTED
-#define OA_PIN_ERISE  PCA9555_P06 // NOT CONNECTED
-#define OA_PIN_SEAST  PCA9555_P07 // NOT CONNECTED
-#define OA_PIN_SOUTH  PCA9555_P10 // OK
-#define OA_PIN_SWEST  PCA9555_P11 // NOT CONNECTED
-#define OA_PIN_WRISE  PCA9555_P12 // NOT CONNECTED
-#define OA_PIN_WDOWN  PCA9555_P13 // NOT CONNECTED
-#define OA_PIN_WFLAT  PCA9555_P14 // OK
-#define OA_PIN_NWEST  PCA9555_P01 // OK
-#define OA_PIN_UPPER  PCA9555_P17 // OK
+#define OA_PIN_LIGHT  PCA9555_P00
+#define OA_PIN_NORTH  PCA9555_P02
+#define OA_PIN_NEAST  PCA9555_P03
+#define OA_PIN_EFLAT  PCA9555_P04
+#define OA_PIN_EDOWN  PCA9555_P05
+#define OA_PIN_ERISE  PCA9555_P06
+#define OA_PIN_SEAST  PCA9555_P07
+#define OA_PIN_SOUTH  PCA9555_P10
+#define OA_PIN_SWEST  PCA9555_P11
+#define OA_PIN_WRISE  PCA9555_P12
+#define OA_PIN_WDOWN  PCA9555_P13
+#define OA_PIN_WFLAT  PCA9555_P14
+#define OA_PIN_NWEST  PCA9555_P01
+#define OA_PIN_UPPER  PCA9555_P17
 
-static VL53L1_Dev_t devNORTH; // 50 FIXME
+static VL53L1_Dev_t devNORTH; // 50 FIXME Removed
 static VL53L1_Dev_t devNEAST; // 51 FIXME
 static VL53L1_Dev_t devEFLAT; // 52 FIXME
-static VL53L1_Dev_t devEDOWN; // 53
-static VL53L1_Dev_t devERISE; // 54
-static VL53L1_Dev_t devSEAST; // 55
+static VL53L1_Dev_t devEDOWN; // 53 FIXME
+static VL53L1_Dev_t devERISE; // 54 TODO Assemble
+static VL53L1_Dev_t devSEAST; // 55 FUNCTIONAL
 static VL53L1_Dev_t devSOUTH; // 56 FIXME
-static VL53L1_Dev_t devSWEST; // 57
-static VL53L1_Dev_t devWRISE; // 58
-static VL53L1_Dev_t devWDOWN; // 59
+static VL53L1_Dev_t devSWEST; // 57 FIXME
+static VL53L1_Dev_t devWRISE; // 58 FUNCTIONAL
+static VL53L1_Dev_t devWDOWN; // 59 FUNCTIONAL
 static VL53L1_Dev_t devWFLAT; // 60 FUNCTIONAL
 static VL53L1_Dev_t devNWEST; // 49 FIXME
 static VL53L1_Dev_t devUPPER; // 61 FUNCTIONAL
@@ -90,17 +90,17 @@ static void oaTask(void *param) {
 
 	systemWaitStart();
 
-	/*VL53L1_StopMeasurement(&devNORTH);VL53L1_StartMeasurement(&devNORTH);*/
+	//VL53L1_StopMeasurement(&devNORTH);VL53L1_StartMeasurement(&devNORTH);
 	/*VL53L1_StopMeasurement(&devNEAST);VL53L1_StartMeasurement(&devNEAST);*/
 	/*VL53L1_StopMeasurement(&devEFLAT);VL53L1_StartMeasurement(&devEFLAT);*/
-	//VL53L1_StopMeasurement(&devEDOWN);VL53L1_StartMeasurement(&devEDOWN);
-	//VL53L1_StopMeasurement(&devERISE);VL53L1_StartMeasurement(&devERISE);
-	//VL53L1_StopMeasurement(&devSEAST);VL53L1_StartMeasurement(&devSEAST);
+	/*VL53L1_StopMeasurement(&devEDOWN);VL53L1_StartMeasurement(&devEDOWN);*/
+	VL53L1_StopMeasurement(&devERISE);VL53L1_StartMeasurement(&devERISE);
+	/*VL53L1_StopMeasurement(&devSEAST);VL53L1_StartMeasurement(&devSEAST);*/
 	/*VL53L1_StopMeasurement(&devSOUTH);VL53L1_StartMeasurement(&devSOUTH);*/
-	//VL53L1_StopMeasurement(&devSWEST);VL53L1_StartMeasurement(&devSWEST);
-	//VL53L1_StopMeasurement(&devWRISE);VL53L1_StartMeasurement(&devWRISE);
-	//VL53L1_StopMeasurement(&devWDOWN);VL53L1_StartMeasurement(&devWDOWN);
-	VL53L1_StopMeasurement(&devWFLAT);VL53L1_StartMeasurement(&devWFLAT);
+	/*VL53L1_StopMeasurement(&devSWEST);VL53L1_StartMeasurement(&devSWEST);*/
+	/*VL53L1_StopMeasurement(&devWRISE);VL53L1_StartMeasurement(&devWRISE);*/
+	/*VL53L1_StopMeasurement(&devWDOWN);VL53L1_StartMeasurement(&devWDOWN);*/
+	/*VL53L1_StopMeasurement(&devWFLAT);VL53L1_StartMeasurement(&devWFLAT);*/
 	/*VL53L1_StopMeasurement(&devNWEST);VL53L1_StartMeasurement(&devNWEST);*/
 	/*VL53L1_StopMeasurement(&devUPPER);VL53L1_StartMeasurement(&devUPPER);*/
 
@@ -108,17 +108,17 @@ static void oaTask(void *param) {
 
 	while (1) {
 		vTaskDelayUntil(&lastWakeTime, M2T(50));
-		/*rNORTH = oaGetMeasurementAndRestart(&devNORTH);*/
+		//rNORTH = oaGetMeasurementAndRestart(&devNORTH);
 		/*rNEAST = oaGetMeasurementAndRestart(&devNEAST);*/
 		/*rEFLAT = oaGetMeasurementAndRestart(&devEFLAT);*/
-		//rEDOWN = oaGetMeasurementAndRestart(&devEDOWN);
-		//rERISE = oaGetMeasurementAndRestart(&devERISE);
-		//rSEAST = oaGetMeasurementAndRestart(&devSEAST);
+		/*rEDOWN = oaGetMeasurementAndRestart(&devEDOWN);*/
+		rERISE = oaGetMeasurementAndRestart(&devERISE);
+		/*rSEAST = oaGetMeasurementAndRestart(&devSEAST);*/
 		/*rSOUTH = oaGetMeasurementAndRestart(&devSOUTH);*/
-		//rSWEST = oaGetMeasurementAndRestart(&devSWEST);
-		//rWRISE = oaGetMeasurementAndRestart(&devWRISE);
-		//rWDOWN = oaGetMeasurementAndRestart(&devWDOWN);
-		rWFLAT = oaGetMeasurementAndRestart(&devWFLAT);
+		/*rSWEST = oaGetMeasurementAndRestart(&devSWEST);*/
+		/*rWRISE = oaGetMeasurementAndRestart(&devWRISE);*/
+		/*rWDOWN = oaGetMeasurementAndRestart(&devWDOWN);*/
+		/*rWFLAT = oaGetMeasurementAndRestart(&devWFLAT);*/
 		/*rNWEST = oaGetMeasurementAndRestart(&devNWEST);*/
 		/*rUPPER = oaGetMeasurementAndRestart(&devUPPER);*/
 	}
@@ -184,31 +184,31 @@ static bool oaTest() {
 
 	pca9555SetOutputRegA(OA_PIN_NWEST);
 	if (vl53l1xInit(&devNWEST, I2C1_DEV)) {
-		DEBUG_PRINT("Init NWEST sensor [OK]\n"); // OK
+		DEBUG_PRINT("Init NWEST sensor [OK]\n");
 	} else {
 		DEBUG_PRINT("Init NWEST sensor [FAIL]\n");
-		pass = false;
+		//pass = false;
 	}
 	pca9555SetOutputRegA(OA_PIN_NORTH);
 	if (vl53l1xInit(&devNORTH, I2C1_DEV)) {
-		DEBUG_PRINT("Init NORTH sensor [OK]\n"); // OK
+		DEBUG_PRINT("Init NORTH sensor [OK]\n");
 	} else {
 		DEBUG_PRINT("Init NORTH sensor [FAIL]\n");
-		pass = false;
+		//pass = false;
 	}
 	pca9555SetOutputRegA(OA_PIN_NEAST);
 	if (vl53l1xInit(&devNEAST, I2C1_DEV)) {
-		DEBUG_PRINT("Init NEAST sensor [OK]\n"); // OK
+		DEBUG_PRINT("Init NEAST sensor [OK]\n");
 	} else {
 		DEBUG_PRINT("Init NEAST sensor [FAIL]\n");
 		//pass = false;
 	}
 	pca9555SetOutputRegA(OA_PIN_EFLAT);
 	if (vl53l1xInit(&devEFLAT, I2C1_DEV)) {
-		DEBUG_PRINT("Init EFLAT sensor [OK]\n"); // OK
+		DEBUG_PRINT("Init EFLAT sensor [OK]\n");
 	} else {
 		DEBUG_PRINT("Init EFLAT sensor [FAIL]\n");
-		pass = false;
+		//pass = false;
 	}
 	pca9555SetOutputRegA(OA_PIN_EDOWN);
 	if (vl53l1xInit(&devEDOWN, I2C1_DEV)) {
@@ -229,12 +229,12 @@ static bool oaTest() {
 		DEBUG_PRINT("Init SEAST sensor [OK]\n");
 	} else {
 		DEBUG_PRINT("Init SEAST sensor [FAIL]\n");
-		//pass = false;
+		pass = false;
 	}
 
 	pca9555SetOutputRegB(OA_PIN_SOUTH);
 	if (vl53l1xInit(&devSOUTH, I2C1_DEV)) {
-		DEBUG_PRINT("Init SOUTH sensor [OK]\n"); // OK
+		DEBUG_PRINT("Init SOUTH sensor [OK]\n");
 	} else {
 		DEBUG_PRINT("Init SOUTH sensor [FAIL]\n");
 		//pass = false;
@@ -251,28 +251,28 @@ static bool oaTest() {
 		DEBUG_PRINT("Init WRISE sensor [OK]\n");
 	} else {
 		DEBUG_PRINT("Init WRISE sensor [FAIL]\n");
-		//pass = false;
+		pass = false;
 	}
 	pca9555SetOutputRegB(OA_PIN_WDOWN);
 	if (vl53l1xInit(&devWDOWN, I2C1_DEV)) {
 		DEBUG_PRINT("Init WDOWN sensor [OK]\n");
 	} else {
 		DEBUG_PRINT("Init WDOWN sensor [FAIL]\n");
-		//pass = false;
+		pass = false;
 	}
 	pca9555SetOutputRegB(OA_PIN_WFLAT);
 	if (vl53l1xInit(&devWFLAT, I2C1_DEV)) {
-		DEBUG_PRINT("Init WFLAT sensor [OK]\n"); // OK
+		DEBUG_PRINT("Init WFLAT sensor [OK]\n");
 	} else {
 		DEBUG_PRINT("Init WFLAT sensor [FAIL]\n");
-		//pass = false;
+		pass = false;
 	}
 	pca9555SetOutputRegB(OA_PIN_UPPER);
 	if (vl53l1xInit(&devUPPER, I2C1_DEV)) {
-		DEBUG_PRINT("Init UPPER sensor [OK]\n"); // OK
+		DEBUG_PRINT("Init UPPER sensor [OK]\n");
 	} else {
 		DEBUG_PRINT("Init UPPER sensor [FAIL]\n");
-		//pass = false;
+		pass = false;
 	}
 
 	isTested = true;
@@ -284,7 +284,7 @@ static const DeckDriver oa_deck = {
 		.vid = 0xBC,
 		.pid = 0x0B,
 		.name = "bcOA",
-		.usedGpio = 0,  // FIXME: set the used pins
+		.usedGpio = 0,
 		.init = oaInit, .test = oaTest, };
 
 DECK_DRIVER(oa_deck);
